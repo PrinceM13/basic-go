42e513bc6403190da7075ce994d748eeb7589614
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-01-29 12:19:20 +0700

    const variable

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var inverstmentAmount, years float64 = 1000, 10
 	expectedReturnRate := 5.5
 
 	futureValue := (inverstmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println("Future value is", futureValue)
+	fmt.Println("Future real value is", futureRealValue)
 }
