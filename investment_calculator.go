2305c87da23c15541878155c8c05b11f976d6a42
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-01-29 12:31:51 +0700

    fetch data from user with fmt-scan

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,8 +7,17 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var inverstmentAmount, years float64 = 1000, 10
+	var inverstmentAmount, years float64
 	expectedReturnRate := 5.5
+
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&inverstmentAmount)
+
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Enter the annual interest rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := (inverstmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
