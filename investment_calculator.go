8d93b928931676ad669916f9160f2ff5edb0c44c
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-05-07 21:59:44 +0700

    create custom function with return value

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,14 +5,15 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
-	var inverstmentAmount, years float64
+	var investmentAmount, years float64
 	expectedReturnRate := 5.5
 
 	// fmt.Print("Enter the investment amount: ")
 	outputText("Enter the investment amount: ")
-	fmt.Scan(&inverstmentAmount)
+	fmt.Scan(&investmentAmount)
 
 	// fmt.Print("Enter the number of years: ")
 	outputText("Enter the number of years: ")
@@ -22,8 +23,9 @@ func main() {
 	outputText("Enter the annual interest rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue := (inverstmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, years, expectedReturnRate)
+	// futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	formattedFV := fmt.Sprintf("Future value is %.2f\n", futureValue)
 	formattedFRV := fmt.Sprintf("Future real value is %.2f\n", futureRealValue)
@@ -33,4 +35,10 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValue(investmentAmount, years, expectedReturnRate float64) (float64, float64) {
+	fv := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
+	frv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, frv
 }
