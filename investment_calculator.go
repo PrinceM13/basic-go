e78900728ba23ff6646b5f4f81551b9ec6d17963
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-05-07 21:42:20 +0700

    create custom function

--- investment_calculator.go
+++ investment_calculator.go
@@ -10,13 +10,16 @@ func main() {
 	var inverstmentAmount, years float64
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter the investment amount: ")
+	// fmt.Print("Enter the investment amount: ")
+	outputText("Enter the investment amount: ")
 	fmt.Scan(&inverstmentAmount)
 
-	fmt.Print("Enter the number of years: ")
+	// fmt.Print("Enter the number of years: ")
+	outputText("Enter the number of years: ")
 	fmt.Scan(&years)
 
-	fmt.Print("Enter the annual interest rate: ")
+	// fmt.Print("Enter the annual interest rate: ")
+	outputText("Enter the annual interest rate: ")
 	fmt.Scan(&expectedReturnRate)
 
 	futureValue := (inverstmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
@@ -26,4 +29,8 @@ func main() {
 	formattedFRV := fmt.Sprintf("Future real value is %.2f\n", futureRealValue)
 
 	fmt.Print(formattedFV, formattedFRV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
